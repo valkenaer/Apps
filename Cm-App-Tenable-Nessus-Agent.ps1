6aa7149d917124c0a2ea41306156246d58f23320
valkenaer <valkenaer@gmail.com>
2026-05-12 10:59:11 +0200

    Add files via upload

---
+++ Cm-App-Tenable-Nessus-Agent.ps1
@@ -0,0 +1,39 @@
+﻿Import-Module $env:SMS_ADMIN_UI_PATH.Replace("\bin\i386","\bin\configurationmanager.psd1")
+$SiteCode = Get-PSDrive -PSProvider CMSITE
+Set-Location "$($SiteCode.Name):\"
+$SiteServer = $Env:COMPUTERNAME
+
+
+$Product     = "Nessus Agent"
+$Arch        = "x64"
+$AppDesc     = "The Nessus platform (officially known as Tenable Nessus) is a vulnerability assessment solution that enables organizations to proactively identify and fix security weaknesses or vulnerabilities across their attack surface before cyberattackers can exploit them."
+$DPGName     = "BSC_DPG"
+$Purp        = "Required"
+$key         = "b274e69840623b7fad37ddd94875ae037631a5f14c86cf02fd5784b750695a29"
+$srvprt      = "10.133.110.12:8834"
+
+ ################################################################################################################################
+
+                           
+                           $SWVersion = "11.1.3"
+                               $fam   = "Clients"                        # Clients or Servers
+                          $nssgrp     = "PROD-CLIENTS-WINDOWS"           # CLIENTS or SERVERS      
+
+#################################################################################################################################
+
+           $MSILocation = "\\$SiteServer\Sources$\Applications\Tenable\Nessus\Agent\$SWVersion\NessusAgent-$SWVersion-x64.msi"
+             $newstr    =  "msiexec.exe /i NessusAgent-$SWVersion-$Arch.msi NESSUS_GROUPS=`"$nssgrp`" NESSUS_SERVER=`"$srvprt`" NESSUS_KEY=$key /qn"
+           $AppFullName = "$Product $SWVersion Windows $fam ($arch)"
+
+
+New-CMApplication -Name $AppFullName -Description $AppDesc -Publisher $PublName -SoftwareVersion $SWVersion
+Add-CMMsiDeploymentType -ApplicationName $AppFullName -ContentLocation $MSILocation -Force
+Set-CMMSIDeploymentType -ApplicationName $AppFullname -DeploymentTypeName "$Product ($Arch) - Windows Installer (*.msi file)" -InstallCommand $newstr -MaximumRuntimeMins 20
+Start-CMContentDistribution -ApplicationName $AppFullName -DistributionPointGroupName $DPGName
+
+
+$DevColl = "Computer Model OptiPlex SFF Plus 7020"
+# $DevColl =   "All Computer with Windows Server **"
+
+New-CMApplicationDeployment -Name $AppFullName -CollectionName $DevColl -DeployAction Install -DeployPurpose $Purp -UserNotification HideAll -AvailableDateTime (Get-Date) -TimeBaseOn LocalTime -Verbose
+
